82c1a7393f6a5716d0d1b6f70759c554e4dab32a
Patrick Meunier <patrick.meunier@me.com>
2018-02-28 14:30:25 -0500

    Initial Commit

---
+++ Ebook Reader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Ebook Reader
+//
+//  Created by Patrick on 2018-02-28.
+//  Copyright © 2018 Patrick Meunier. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Ebook Reader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ebook Reader
+//
+//  Created by Patrick on 2018-02-28.
+//  Copyright © 2018 Patrick Meunier. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
